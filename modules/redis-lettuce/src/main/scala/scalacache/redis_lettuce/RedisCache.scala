8b3a87c64442a0829be51123453468dbe13a9375
Paul Danilin <14351112+Paulymorph@users.noreply.github.com>
2020-05-03 17:06:17 +0300

    Add redis cluster implementation

--- modules/redis-lettuce/src/main/scala/scalacache/redis_lettuce/RedisCache.scala
+++ modules/redis-lettuce/src/main/scala/scalacache/redis_lettuce/RedisCache.scala
@@ -3,21 +3,31 @@ package scalacache.redis_lettuce
 import java.util.concurrent.CompletionStage
 
 import io.lettuce.core.api.StatefulRedisConnection
-import io.lettuce.core.api.async.RedisAsyncCommands
+import io.lettuce.core.cluster.api.StatefulRedisClusterConnection
+import io.lettuce.core.cluster.api.async.RedisClusterAsyncCommands
+import io.lettuce.core.cluster.{ClusterClientOptions, RedisClusterClient}
 import io.lettuce.core.codec.RedisCodec
 import io.lettuce.core.masterreplica.MasterReplica
 import io.lettuce.core.resource.ClientResources
 import io.lettuce.core.support.{AsyncConnectionPoolSupport, AsyncPool, BoundedAsyncPool, BoundedPoolConfig}
 import io.lettuce.core.{ClientOptions, ReadFrom, RedisClient, RedisURI, SetArgs}
 import scalacache.logging.Logger
+import scalacache.redis_lettuce.RedisCache.CommandsExtractor
 import scalacache.{AbstractCache, Cache, CacheConfig, Mode}
 
 import scala.concurrent.duration.Duration
 
-class RedisCache[V] private(
-                             pool: AsyncPool[StatefulRedisConnection[String, V]],
-                             resourcesCleanup: () => Unit
-                           )(implicit val config: CacheConfig) extends AbstractCache[V] {
+// You need to have a strange "Connection" type as StatefulRedisConnection and StatefulRedisClusterConnection
+// do not have common ancestor with async() method. Though they both have the method.
+// So you cannot have a pool typed AsyncPool[SomeBaseConnection] that
+// would work for both types: StatefulRedisConnection and StatefulRedisClusterConnection
+class RedisCache[V, Connection] private(
+                                         pool: AsyncPool[Connection],
+                                         resourcesCleanup: () => Unit
+                                       )(
+                                         implicit val config: CacheConfig,
+                                         commandsExtractor: CommandsExtractor[Connection, V]
+                                       ) extends AbstractCache[V] {
   override protected def doGet[F[_]](key: String)(implicit mode: Mode[F]): F[Option[V]] =
     withCommands { commands =>
       commands.get(key).thenApply { value =>
@@ -51,10 +61,10 @@ class RedisCache[V] private(
 
   override def close[F[_]]()(implicit mode: Mode[F]): F[Any] = mode.M.delay(resourcesCleanup(): Any)
 
-  private def withCommands[F[_], T](f: RedisAsyncCommands[String, V] => CompletionStage[T])
+  private def withCommands[F[_], T](f: RedisClusterAsyncCommands[String, V] => CompletionStage[T])
                                    (implicit mode: Mode[F]): F[T] = mode.M.async { callback =>
     pool.acquire().thenCompose { connection =>
-      val commands = connection.async()
+      val commands = commandsExtractor.async(connection)
       f(commands).thenCompose { result =>
         callback(Right(result))
         pool.release(connection)
@@ -117,4 +127,33 @@ object RedisCache {
       client.shutdown()
     })
   }
+
+  def cluster[V](redisUri: RedisURI, otherRedisUris: RedisURI*)(
+    clientResources: ClientResources,
+    poolConfig: BoundedPoolConfig,
+    clientOptions: ClusterClientOptions = ClusterClientOptions.create()
+  )(implicit codec: RedisCodec[String, V], cacheConfig: CacheConfig): Cache[V] = {
+    val client = RedisClusterClient.create(clientResources, (redisUri +: otherRedisUris).asJava)
+    client.setOptions(clientOptions)
+    val pool: BoundedAsyncPool[StatefulRedisClusterConnection[String, V]] =
+      AsyncConnectionPoolSupport.createBoundedObjectPool(
+        () => client.connectAsync(codec),
+        poolConfig
+      )
+    new RedisCache(pool, () => {
+      pool.close()
+      client.shutdown()
+    })
+  }
+
+  private implicit def redisSimpleConnectionCommandsExtractor[V]: CommandsExtractor[StatefulRedisConnection[String, V], V] =
+    connection => connection.async()
+
+  private implicit def redisClusterConnectionCommandsExtractor[V]: CommandsExtractor[StatefulRedisClusterConnection[String, V], V] =
+    connection => connection.async()
+
+  private trait CommandsExtractor[Connection, V] {
+    def async(connection: Connection): RedisClusterAsyncCommands[String, V]
+  }
+
 }
